13e8f701f070f6f346846eacaba100c1e153b69e
rigobertoalag <a20150338@utem.edu.mx>
2021-10-05 13:40:51 -0500

    Primer acercamiento a Jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,39 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { create } from 'react-test-renderer'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+let component
+let header
+
+describe("<App />", () => {
+
+  beforeAll(()=>{
+    component = create(<App />)
+    header = component.root.findByType('header')
+  })
+
+  it("Renderiza el componente", () => {
+    expect(component).toBeDefined();
+  })
+
+  it("Renderiza el header", () => {
+    expect(header).toBeDefined()
+    expect(header.props.className).toEqual("App-header")
+  })
+
+  it("renderiza un img, un p y un a", () => {
+    const img = header.findByType('img')
+    const p = header.findByType('p')
+    const a = header.findByType('a')
+
+    expect(img).toBeDefined()
+    expect(img.props.className).toEqual("App-logo")
+    expect(img.props.alt).toEqual("logo")
+    expect(img.props.src).toEqual("logo.svg")
+
+    expect(a).toBeDefined()
+    expect(a.props.href).toBe("https://reactjs.org")
+
+    expect(p).toBeDefined()
+    expect(p.findByType("code")).toBeDefined()
+  })
+})
